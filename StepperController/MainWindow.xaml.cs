f8cadb46de02ae00ea8b7b8b458a22fc0e93ff27
César Zárate <cesarjzo@outlook.com>
2022-11-22 10:32:40 -0600

    Add stop button and serial connection

--- StepperController/MainWindow.xaml.cs
+++ StepperController/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using StepperController.Serial;
 
 namespace StepperController
 {
@@ -20,39 +21,53 @@ namespace StepperController
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly SerialConnection _serial;
         public MainWindow()
         {
             InitializeComponent();
+            _serial = new SerialConnection();
         }
 
         private void BtnSimpleCw_OnClick(object sender, RoutedEventArgs e)
         {
             TxtMode.Text = "Simple step (clockwise)";
+            _serial.SendMessage("scw");
         }
 
         private void BtnSimpleCc_OnClick(object sender, RoutedEventArgs e)
         {
             TxtMode.Text = "Simple step (counterclockwise)";
+            _serial.SendMessage("scc");
         }
 
         private void BtnFullCw_OnClick(object sender, RoutedEventArgs e)
         {
             TxtMode.Text = "Full step (clockwise)";
+            _serial.SendMessage("fcw");
         }
 
         private void BtnFullCc_OnClick(object sender, RoutedEventArgs e)
         {
             TxtMode.Text = "Full step (counterclockwise)";
+            _serial.SendMessage("fcc");
         }
 
         private void BtnHalfCw_OnClick(object sender, RoutedEventArgs e)
         {
             TxtMode.Text = "Half step (clockwise)";
+            _serial.SendMessage("hcw");
         }
 
         private void BtnHalfCc_OnClick(object sender, RoutedEventArgs e)
         {
             TxtMode.Text = "Half step (counterclockwise)";
+            _serial.SendMessage("hcc");
+        }
+
+        private void BtnStop_OnClick(object sender, RoutedEventArgs e)
+        {
+            TxtMode.Text = "";
+            _serial.SendMessage("stop");
         }
     }
 }
---
+++ StepperController/Serial/SerialConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace StepperController.Serial
+{
+    public class SerialConnection
+    {
+        public SerialPort Port { get; }
+
+        public SerialConnection()
+        {
+            Port = new SerialPort("COM6", 9600);
+            Port.Open();
+        }
+
+        public void SendMessage(string message)
+        {
+            if (Port.IsOpen)
+                Port.Write(message);
+        }
+    }
+}
